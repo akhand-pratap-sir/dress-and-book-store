97ff3212870ec4d3f51051673e376a32e6eb5962
akhand-pratap-sir <157180083+akhand-pratap-sir@users.noreply.github.com>
2024-08-03 08:03:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ function validateLogin() {
     const username = document.getElementById('username').value;
     const password = document.getElementById('password').value;
 
-    if (username === 'anuragyadavakhand@gmail.com' && password === 'hagalbaba.com') {
+    if (username === 'mdic' && password === '12345') {
        // window.location.href = 'indexx.html'; 
         window.open('indexx.html', '_blank'); // Update to your main page URL if needed
     } else {
